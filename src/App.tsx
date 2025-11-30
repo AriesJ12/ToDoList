e1d4a3fd89df5041a0d79873bc63aa802cc0b3c2
AriesJ <ajtagle12@gmail.com>
2025-11-30 17:26:43 +0800

    making the popup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,39 @@
+import { DndContext } from "@dnd-kit/core";
 import "./App.css";
+import { useEffect } from "react";
+import { useTaskStore } from "./hooks/task";
+import ItemsCategories from "./components/ItemsCategories";
+import { Status } from "./types";
+import Items from "./components/Items";
 
 function App() {
+  const tasks = useTaskStore((s) => s.tasks);
+  // const updateTask = useTaskStore((s) => s.update);
+  const loadTask = useTaskStore((s) => s.load);
+  // const removeTask = useTaskStore((s) => s.remove);
+
+  const status = Status[1];
+
+  useEffect(() => {
+    loadTask();
+  }, [loadTask]);
+
   return (
     <>
-      <button type="button" onClick={openMainWindow} className="">open window</button>
+      <button type="button" onClick={openMainWindow} className="">
+        open window
+      </button>
+      <DndContext>
+        <main>
+          <ItemsCategories id={status} key={status}>
+            {tasks
+              .filter((task) => task.status === status)
+              .map((task) => (
+                <Items key={task.id} id={task.id} value={task.details} />
+              ))}
+          </ItemsCategories>
+        </main>
+      </DndContext>
     </>
   );
 
