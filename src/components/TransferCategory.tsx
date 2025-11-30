dc1b5368611a14d18f1bf02fe64c2371d6808519
AriesJ <ajtagle12@gmail.com>
2025-11-30 15:56:50 +0800

    added transfer category

--- src/components/TransferCategory.tsx
+++ src/components/TransferCategory.tsx
@@ -1,18 +1,49 @@
 import { useDroppable } from "@dnd-kit/core";
+import { type HTMLAttributes, type ReactNode } from "react";
 
-interface TransferCategoryProps {
+interface TransferCategoryProps extends HTMLAttributes<HTMLDivElement> {
   id: string;
+  mode: "left" | "right";
+  children: ReactNode;
 }
 
-function TransferCategory(props: TransferCategoryProps) {
+function TransferCategory({
+  id,
+  mode,
+  children,
+  className,
+  ...props
+}: TransferCategoryProps) {
   const { isOver, setNodeRef } = useDroppable({
-    id: props.id,
+    id: id,
   });
 
   const style = {
     color: isOver ? "green" : undefined,
   };
-  return <div ref={setNodeRef} style={style}>TransferCategory</div>;
+
+  return (
+    <div
+      ref={setNodeRef}
+      style={style}
+      className={getClassName(mode)}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+
+  function getClassName(mode: "left" | "right") {
+    let className = "";
+
+    if (mode === "left") {
+      className = "absolute left-0 rounded-r-lg";
+    } else if (mode === "right") {
+      className = "absolute right-0 rounded-l-lg";
+    }
+
+    return className;
+  }
 }
 
 export default TransferCategory;
